47b7abdabd0dc6f52c02bfc2829621e9a89bc2ca
yajuchan <09tanakazu@gmail.com>
2026-04-17 14:27:21 +0900

    add

--- .bashrc
+++ .bashrc
@@ -5,10 +5,23 @@
 # If not running interactively, don't do anything
 [[ $- != *i* ]] && return
 
-alias ls='ls --color=auto'
-alias ls='ls -alF'
+
+alias ls='ls --color=auto -alF'
 alias rm='rm -rf'
 alias mkdir='mkdir -p'
 alias cp='cp -r'
+alias vi='nvim'
 alias grep='grep --color=auto'
 PS1='[\u@\h \W]\$ '
+export PATH=$PATH:/opt/watcom/binl64:~/.cargo/bin:~/.ghcup/bin
+export WATCOM=/opt/watcom
+. "$HOME/.cargo/env"
+
+export DEVKITPRO=/opt/devkitpro 
+export DEVKITARM=${DEVKITPRO}/devkitARM 
+export DEVKITPPC=${DEVKITPRO}/devkitPPC
+export PATH=${DEVKITPRO}/tools/bin:$PATH 
+
+source /usr/share/nvm/init-nvm.sh
+
+[ -f "/home/yajusenpai/.ghcup/env" ] && . "/home/yajusenpai/.ghcup/env" # ghcup-env
--- first.sh
+++ first.sh
@@ -7,6 +7,18 @@ cd ./yay/
 makepkg -si
 mkdir /mnt/hgfs/
 yay -S cups ghostscript gsfonts epson-printer-utility epson-inkjet-printer-escpr2 epsonscan2-bin
+sudo pacman-key --recv BC26F752D25B92CE272E0F44F7FD5492264BB9D0 --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign BC26F752D25B92CE272E0F44F7FD5492264BB9D0
+sudo pacman -U https://pkg.devkitpro.org/devkitpro-keyring.pkg.tar.zst
+sudo pacman-key --populate devkitpro
+
+sudo cat << EOF >> /etc/pacman.conf
+[dkp-libs]
+Server = https://pkg.devkitpro.org/packages
+[dkp-linux]
+Server = https://pkg.devkitpro.org/packages/linux/$arch/
+EOF
+
 yay -S --needed < pkglist.txt
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
